5265bf532bf4f9347c9d3feea489c2e8a85db65b
saksham03 <38920222+Saksham033@users.noreply.github.com>
2018-11-22 23:38:11 +0530

    Make bild environment setup script

---
+++ build_env.sh
@@ -0,0 +1,55 @@
+#Script For setting up build environment....
+
+echo Search for packages
+
+sudo apt update && sudo apt upgrade
+
+echo done
+
+#install git-core
+
+echo installing
+
+sudo apt-get install git-core
+
+echo done
+
+#Cloning scripts
+
+echo cloning
+
+git clone https://github.com/akhilnarang/scripts.git
+
+echo done 
+
+#Running and exporting scripts...
+
+echo Working
+
+cd scripts && bash s*/a*
+
+echo done
+
+#Make bin directory to initialise repo their
+
+echo making "Bin"
+
+mkdir -p ~/bin
+
+echo done
+
+#Cloning Repo from googleapi
+
+echo cloning
+
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+
+echo done
+
+#Give permissions
+
+echo Wait
+
+chmod +x ~/bin/repo
+
+echo done. Thanks for using :-)
